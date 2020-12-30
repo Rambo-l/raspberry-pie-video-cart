01e65df6162e1ed7c6c736bc8c5cfc6de0eb3821
Liang <15208219794@163.com>
2020-12-30 19:47:16 +0800

    ps pro

--- ps2_control.py
+++ ps2_control.py
@@ -7,7 +7,29 @@ import os
 os.environ['SDL_VIDEODRIVE'] = 'x11'
 from time import ctime,sleep,time
 
+#手柄按键定义
+PSB_SELECT = 1
+PSB_L3 =  2
+PSB_R3 =  3
+PSB_START = 4
+PSB_PAD_UP = 5
+PSB_PAD_RIGHT = 6
+PSB_PAD_DOWN  = 7
+PSB_PAD_LEFT  = 8
+PSB_L2 = 9
+PSB_R2 = 10
+PSB_L1 = 11
+PSB_R1 = 12
+PSB_TRIANGLE = 13
+PSB_CIRCLE = 14
+PSB_CROSS  = 15
+PSB_SQUARE = 16
 
+#回发过来的后4个数据是摇杆的数据
+PSS_RX = 5    #右摇杆X轴数据
+PSS_RY = 6    #右摇杆Y轴数据
+PSS_LX = 7    #左摇杆X轴数据
+PSS_LY = 8    #右摇杆Y轴数据
 
 #PS2引脚设置,接树莓派引脚
 PS2_DAT_PIN = 10 #MOS
@@ -15,13 +37,6 @@ PS2_CMD_PIN = 9  #MIS
 PS2_SEL_PIN = 25 #CS  
 PS2_CLK_PIN = 11 #SCK
 
-
-
-global PS2_KEY
-global X1
-global Y1
-global X2
-global Y2
 global Handkey
 scan=[0x01,0x42,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
 Data=[0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]
@@ -29,7 +44,6 @@ MASK = [PSB_SELECT,PSB_L3,PSB_R3,PSB_START,PSB_PAD_UP,PSB_PAD_RIGHT,PSB_PAD_DOWN
 
 #设置GPIO口为BCM编码方式
 GPIO.setmode(GPIO.BCM)
-
 #忽略警告信息
 GPIO.setwarnings(False)
 
